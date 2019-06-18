381737c993b841361608d4ebf41470f4321ded58
Alden <aldentan97@gmail.com>
2019-06-18 10:46:50 +0800

    Refactored write_notification_when_autograded and write_notification_when_manually_graded in Cadet.Accounts.Notification. Also started work on a function for new assessments.

--- lib/cadet/accounts/notification_type.ex
+++ lib/cadet/accounts/notification_type.ex
@@ -1,15 +1,18 @@
 import EctoEnum
 
 defenum(Cadet.Accounts.NotificationType, :notification_type, [
-  # Notifications for New assessments
+  # Notifications for new assessments
   :new,
+
   # Notifications for deadlines
   :deadline,
+
   # Notifications for autograded assessments
   :autograded,
-  # Notifications for manually graded assessments
-  :graded,
 
-  # Notifications for Submitted assessments
+  # Notifications for manually graded assessments
+  :manually_graded,
+
+  # Notifications for submitted assessments
   :submitted
 ])
